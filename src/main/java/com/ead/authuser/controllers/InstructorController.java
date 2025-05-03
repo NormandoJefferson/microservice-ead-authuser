56f946b518edbe849ba4356fc5f571979132e414
NormandoJefferson <normandojefferson@hotmail.com>
2025-05-03 12:09:21 -0300

    feat(state-transfer): update saveSubscriptionInstructor method

--- src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -1,6 +1,7 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.InstructorDto;
+import com.ead.authuser.enums.ActionType;
 import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
@@ -24,6 +25,21 @@ public class InstructorController {
     @Autowired
     UserService userService;
 
+    /**
+     * Inscreve um usuário existente como instrutor.
+     * <p>
+     * Este endpoint recebe um objeto {@link InstructorDto}, contendo o ID de um usuário previamente cadastrado,
+     * e atualiza seu tipo para {@link UserType#INSTRUCTOR}. Também define a data da última atualização para o momento atual
+     * e persiste as alterações no banco de dados.
+     * <p>
+     * A operação de atualização aciona a publicação de um evento no RabbitMQ com a ação
+     * {@link ActionType#UPDATE}, por meio do  {@link UserService#updateUser(UserModel)}.
+     * <p>
+     * Caso o usuário informado não seja encontrado, retorna {@code 404 Not Found}.
+     *
+     * @param instructorDto o DTO contendo o ID do usuário a ser promovido para instrutor
+     * @return {@code 200 OK} com o objeto {@link UserModel} atualizado, ou {@code 404 Not Found} caso o usuário não exista
+     */
     @PostMapping("/subscription")
     public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto) {
         Optional<UserModel> userModelOptional = userService.findById(instructorDto.getUserId());
@@ -33,7 +49,7 @@ public class InstructorController {
             var userModel = userModelOptional.get();
             userModel.setUserType(UserType.INSTRUCTOR);
             userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
-            userService.save(userModel);
+            userService.updateUser(userModel);
             return ResponseEntity.status(HttpStatus.OK).body(userModel);
         }
     }
